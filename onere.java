12fa2415a78a3f1827dfa95a9c1adc05b3a8782a
Anitha <anithachandran599@gmail.com>
2017-09-01 14:24:25 +0530

    Create onere.java

---
+++ onere.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+class onere
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        int count=0;
+        for (int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+            if(a[i]!=a[j])
+            {
+              count++;
+            }
+            }
+            if(count==(n-1))
+            {
+                System.out.print(a[i]);
+            }
+            count=0;
+        }
+    }
+}
+
+
+
+
